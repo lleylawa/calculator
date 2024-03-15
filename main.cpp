60251f3ac357cbc3cec45e7f09eeaff5204e4230
lleylawa <150817035+lleylawa@users.noreply.github.com>
2024-03-15 11:03:40 +0600

    main.cpp

--- main.cpp
+++ main.cpp
@@ -4,165 +4,260 @@
 #include <stack>
 using namespace std;
 
-struct Leksema {
-    char type;
-    double value;
+struct Leksema //Структура, описывающая любое число или операцию
+{
+	char type; // 0 для чисел, "+" для операции сложения и т.д.
+	double value; //Значение (только для чисел). У операций значение всегда "0"
 };
 
-bool Maths(stack <Leksema>& Stack_n, stack <Leksema>& Stack_o, Leksema& item) {
-    double a, b, c;
-    a = Stack_n.top().value;
-    Stack_n.pop();
-    switch (Stack_o.top().type) {
-    case '+':
-        b = Stack_n.top().value;
-        Stack_n.pop();
-        c = a + b;
-        break;
-    case '-':
-        b = Stack_n.top().value;
-        Stack_n.pop();
-        c = b - a;
-        break;
-    case '^':
-        b = Stack_n.top().value;
-        Stack_n.pop();
-        c = pow(b, a);
-        break;
-    case '*':
-        b = Stack_n.top().value;
-        Stack_n.pop();
-        c = a * b;
-        break;
-    case '/':
-        b = Stack_n.top().value;
-        Stack_n.pop();
-        if (a == 0) {
-            cerr << "Error! Cannot divide by 0!" << endl;
-            return false;
-        }
-        c = b / a;
-        break;
-    case 'e':
-        c = exp(a);
-        break;
-    case 's':
-        c = sqrt(a);
-        break;
-    case 'r':
-        c = round(a);
-        break;
-    case 'a':
-        c = abs(a);
-        break;
-    default:
-        cerr << "Error!" << endl;
-        return false;
-    }
-    item.type = '0';
-    item.value = c;
-    Stack_n.push(item);
-    Stack_o.pop();
-    return true;
+bool Maths(stack <Leksema>& Stack_n, stack <Leksema>& Stack_o, Leksema& item) { //Математическая функция, которая производит расчеты
+	//Функция имеет тип bool, чтобы при возникновении какой-либо ошибки возвращать "false"
+	double a, b, c;
+	a = Stack_n.top().value; //Берется верхнее число из стека с числами
+	Stack_n.pop(); //Удаляется верхнее число из стека с числами
+	switch (Stack_o.top().type) {  //Проверяется тип верхней операции из стека с операциями
+	case '+': //Если тип верхней операции из стека с операциями сложение
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = a + b;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item); //Результат операции кладется обратно в стек с числами
+		Stack_o.pop();
+		break;
+
+	case '-':
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = b - a;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+
+	case '^':
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = pow(b, a);
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+
+	case '*':
+		b = Stack_n.top().value;
+		Stack_n.pop();
+		c = a * b;
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+
+	case '/':
+		b = Stack_n.top().value;
+		if (a == 0) {
+			cerr << "Error! Cannot divide by 0!" << endl;
+			return false;
+		}
+		else {
+			Stack_n.pop();
+			c = (b / a);
+			item.type = '0';
+			item.value = c;
+			Stack_n.push(item);
+			Stack_o.pop();
+			break;
+			
+	case 'e':
+		c = exp(a);
+		item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+		
+	case 's':
+	    c = sqrt(a);
+	    item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+		
+	case 'r':
+	    c = round(a);
+	    item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+		
+	case 'a':
+	    c = abs(a);
+	    item.type = '0';
+		item.value = c;
+		Stack_n.push(item);
+		Stack_o.pop();
+		break;
+		
+	default:
+		cerr << "Error!" << endl;
+		return false;
+		break;
+		}
+	}
+	return true;
 }
 
-int getRang(char Ch) {
-    if (Ch == 'e' || Ch == 's' || Ch == 'r' || Ch == 'a') return 4;
-    if (Ch == '^') return 3;
-    if (Ch == '+' || Ch == '-') return 1;
-    if (Ch == '*' || Ch == '/') return 2;
-    return 0;
+int getRang(char Ch) { //Функция возвращает приоритет операции: "1" для сложения и вычитания, "2" для умножения и деления и т.д.
+	if (Ch == 'e' || Ch == 's' || Ch == 'r' || Ch == 'a')return 4;
+	if (Ch == '^')return 3;
+	if (Ch == '+' || Ch == '-')return 1;
+	if (Ch == '*' || Ch == '/')return 2;
+	else return 0;
 }
 
-int main() {
-    cout << "Welcome to the Calculator!" << endl;
-    cout << "Please enter your arithmetic expression: ";
-    while (true) {
+int main()
+{
+     cout << "Welcome to the Calculator!" << endl;
+     cout << "Please enter your arithmetic expression: ";
+     while (true) {
         system("cls");
-        string str;
+        std::string str;
         getline(cin, str);
-        stringstream sstr{ str };
+        stringstream sstr{str};
 
-        char Ch;
+        char Ch; //Переменная, в которую будет записываться текущий обрабатываемый символ
         double value;
-        bool unary_minus_flag = true;
-        stack<Leksema> Stack_n;
-        stack<Leksema> Stack_o;
-        Leksema item;
-        while (sstr >> Ch) {
-            if (Ch == ' ') {
+        bool flag = true; //Нужен для того, чтобы программа смогла отличить унарный минус (-5) от вычитания (2-5)
+        stack<Leksema> Stack_n; //Стек с числами
+        stack<Leksema> Stack_o; //Стек с операциями
+        Leksema item; //Объект типа Leksema
+        while (true) {
+            Ch = sstr.peek(); //Смотрим на первый символ
+            if (Ch == '\377')break; //Если достигнут конец строки, выходим из цикла
+            if (Ch == ' ') { //Игнорирование пробелов
+                sstr.ignore();
                 continue;
             }
-            if (Ch == 'e' || Ch == 's' || Ch == 'r' || Ch == 'a') {
-                string func;
-                func.push_back(Ch);
-                sstr >> Ch;
-                func.push_back(Ch);
-                sstr >> Ch;
-                func.push_back(Ch);
-                if (func == "exp") {
+            if (Ch == 'e' || Ch == 'a') { //Если прочитана функция
+                char foo[3];
+                for (int i = 0; i < 3; i++) {
+                    Ch = sstr.peek();
+                    foo[i] = Ch;
+                    sstr.ignore();
+                }
+                if (foo[0] == 'e' && foo[1] == 'x' && foo[2] == 'p') {
                     item.type = 'e';
+                    item.value = 0;
+                    Stack_o.push(item);
+                    continue;
                 }
-                else if (func == "abs") {
-                    item.type = 'a';
+                if (foo[0] == 'a' && foo[1] == 'b' && foo[2] == 's') {
+                    double operand;
+                    sstr >> operand; // Read the operand of abs(a) function
+                    item.type = 'a'; // Indicate that it's an abs(a) function
+                    item.value = operand;
+                    Stack_n.push(item); // Push the operand onto the number stack
+                    continue;
                 }
-                else if (func == "sqrt") {
-                    item.type = 's';
-                }
-                else if (func == "round") {
-                    item.type = 'r';
-                }
-                item.value = 0;
-                Stack_o.push(item);
-                continue;
             }
-            if (Ch >= '0' && Ch <= '9' || Ch == '-' && unary_minus_flag) {
-                sstr.putback(Ch);
+            if (Ch == 's') { //Если прочитана функция    
+                char foo[4];
+                for (int i = 0; i < 4; i++) {
+                    Ch = sstr.peek();
+                    foo[i] = Ch;
+                    sstr.ignore();
+                }
+                if (foo[0] == 's' && foo[1] == 'q' && foo[2] == 'r' && foo[3] == 't') {
+                    item.type = 's';
+                    item.value = 0;
+                    Stack_o.push(item); //Операция кладется в стек с операциями
+                    continue;
+                }
+            }
+            if (Ch == 'r') { //Если прочитана функция
+                char foo[5]; 
+                for (int i = 0; i < 5; i++) {
+                    Ch = sstr.peek();
+                    foo[i] = Ch;
+                    sstr.ignore();
+                }
+                if (foo[0] == 'r' && foo[1] == 'o' && foo[2] == 'u' && foo[3] == 'n' && foo[4] == 'd') {
+                    item.type = 'r';
+                    item.value = 0;
+                    Stack_o.push(item); //Операция кладется в стек с операциями
+                    continue;
+                }             
+            }    
+            if (Ch >= '0' && Ch <= '9' || Ch == '-' && flag == 1) { //Если прочитано число
                 sstr >> value;
                 item.type = '0';
                 item.value = value;
-                Stack_n.push(item);
-                unary_minus_flag = false;
+                Stack_n.push(item); //Число кладется в стек с числами
+                flag = 0;
                 continue;
             }
-            if (Ch == '+' || Ch == '-' || Ch == '*' || Ch == '/' || Ch == '^') {
-                while (!Stack_o.empty() && getRang(Ch) <= getRang(Stack_o.top().type)) {
-                    if (!Maths(Stack_n, Stack_o, item)) {
-                        return 0;
-                    }
+            if (Ch == '+' || Ch == '-' && flag == 0 || Ch == '*' || Ch == '/' || Ch == '^') { //Если прочитана операция
+                if (Stack_o.size() == 0) { //Если стек с операциями пуст
+                    item.type = Ch;
+                    item.value = 0;
+                    Stack_o.push(item); //Операция кладется в стек с операциями
+                    sstr.ignore();
+                    continue;
                 }
-                item.type = Ch;
-                item.value = 0;
-                Stack_o.push(item);
-                unary_minus_flag = true;
-                continue;
-            }
-            if (Ch == '(') {
-                item.type = Ch;
-                item.value = 0;
-                Stack_o.push(item);
-                unary_minus_flag = true;
-                continue;
-            }
-            if (Ch == ')') {
-                while (!Stack_o.empty() && Stack_o.top().type != '(') {
-                    if (!Maths(Stack_n, Stack_o, item)) {
+                if (Stack_o.size() != 0 && getRang(Ch) > getRang(Stack_o.top().type)) { //Если стек с операциями НЕ пуст, но приоритет текущей операции выше верхней в стеке с операциями
+                    item.type = Ch;
+                    item.value = 0;
+                    Stack_o.push(item); //Операция кладется в стек с операциями
+                    sstr.ignore();
+                    continue;
+                }
+                if (Stack_o.size() != 0 && getRang(Ch) <=
+                                           getRang(Stack_o.top().type)) {//Если стек с операциями НЕ пуст, но приоритет текущей операции ниже либо равен верхней в стеке с операциями
+                    if (Maths(Stack_n, Stack_o, item) == false) { //Если функция вернет "false", то прекращаем работу
+                        system("pause");
                         return 0;
                     }
+                    continue;
+                }
+            }
+            if (Ch == '(') { //Если прочитана открывающаяся скобка
+                item.type = Ch;
+                item.value = 0;
+                Stack_o.push(item); //Операция кладется в стек с операциями
+                sstr.ignore();
+                continue;
+            }
+            if (Ch == ')') { //Если прочитана закрывающаяся скобка
+                while (Stack_o.top().type != '(') {
+                    if (Maths(Stack_n, Stack_o, item) == false) { //Если функция вернет "false", то прекращаем работу
+                        system("pause");
+                        return 0;
+                    } else continue; //Если все хорошо
                 }
                 Stack_o.pop();
+                sstr.ignore();
                 continue;
-            }
-            cout << "Expression entered incorrectly!" << endl;
-            return 0;
-        }
-        while (!Stack_o.empty()) {
-            if (!Maths(Stack_n, Stack_o, item)) {
+            } else { //Если прочитан какой-то странный символ
+                cout << "Expression entered incorrectly!" << endl;
+                system("pause");
                 return 0;
             }
         }
+        while (Stack_o.size() !=
+               0) { //Вызываем матем. функцию до тех пор, пока в стеке с операциями не будет 0 элементов
+            if (Maths(Stack_n, Stack_o, item) == false) { //Если функция вернет "false", то прекращаем работу
+                system("pause");
+                return 0;
+            } else continue; //Если все хорошо
+        }
         cout << "Result: " << Stack_n.top().value << endl;
         cout << "Thank you for using the Calculator!" << endl;
+        system("pause");
     }
     return 0;
 }
